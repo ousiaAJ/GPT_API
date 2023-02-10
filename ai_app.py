af4d892d01866f84a987f82087b99a4b65a23b4e
ousiaAJ <101981464+ousiaAJ@users.noreply.github.com>
2023-02-10 10:18:47 +0100

    initial commit

---
+++ ai_app.py
@@ -0,0 +1,43 @@
+import os
+import openai
+import json
+from flask import Flask, render_template, request
+
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+  return render_template("index.html")
+
+@app.route('/result', methods=['POST'])
+def result():
+  auftrag = request.form["textinput"]
+  if auftrag:
+      result = gpt(auftrag)
+      return render_template("result.html", result=result)
+  else:
+      return render_template("index.html")
+
+
+
+
+
+def gpt(auftrag):
+  print("API started")
+  openai.api_key = "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"
+
+  response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt=auftrag,
+    temperature=0.6,
+    max_tokens=2000,
+    top_p=1,
+    frequency_penalty=1,
+    presence_penalty=1
+  )
+  json_object = json.loads(str(response))
+  answer = json_object['choices'][0]['text']
+  return answer
+
+if __name__ == '__main__':
+    app.run(debug=True)
